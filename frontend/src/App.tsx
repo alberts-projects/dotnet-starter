f5543ed84e0aabce3424dc1d48d161f60946ce5e
albertchua-bt <Albert.Chua@bakertilly.com>
2023-09-21 10:15:37 -0400

    adds react bootstrap and bootstrap to repo for examples

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,9 @@
-import './App.css'
-import { TabsDemo } from './components/tabsdemo';
+import React, { useState } from 'react';
+
 import Videos from './components/videos';
+
+
+// shadui imports
 import {
   Accordion,
   AccordionContent,
@@ -8,7 +11,32 @@ import {
   AccordionTrigger,
 } from "@/components/ui/accordion"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { TabsDemo } from './components/tabsdemo';
 
+// bootstrap imports
+import Toast from 'react-bootstrap/Toast';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+
+// css
+import './App.css'
+
+
+const ExampleToast = ({ children }) => {
+  const [show, toggleShow] = useState(true);
+
+  return (
+    <>
+      {!show && <Button onClick={() => toggleShow(true)}>Show Toast</Button>}
+      <Toast show={show} onClose={() => toggleShow(false)}>
+        <Toast.Header>
+          <strong className="mr-auto">React-Bootstrap</strong>
+        </Toast.Header>
+        <Toast.Body>{children}</Toast.Body>
+      </Toast>
+    </>
+  );
+};
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -36,6 +64,19 @@ function App() {
 
       <TabsDemo />
 
+            {/* bootstrap example */}
+            <Container className="p-3">
+        <Container className="p-5 mb-4 bg-light rounded-3">
+          <h1 className="header">Welcome To React-Bootstrap</h1>
+          <ExampleToast>
+            We now have Toasts
+            <span role="img" aria-label="tada">
+              🎉
+            </span>
+          </ExampleToast>
+        </Container>
+      </Container>
+      
     </>
 
     
